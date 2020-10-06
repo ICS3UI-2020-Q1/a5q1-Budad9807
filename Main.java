01b4c109e147b4af0e7dd5d108142e0008bc746e
Budad9807 <Budad9807@wrdsb.ca>
2020-10-06 15:05:13 +0000

    i made multiplication table

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *When a user types a interger creates a multiplication table
+ * @author David
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // Asking the user to enter a interger
+    System.out.println("Please enter a interger");
+    int num = input.nextInt();
+   // create a loop the creates a multiplication table when interger is entered
+   for(int count =1; count <= 12; count++){
+     int j = num * count;
+     // it loop the interger that is enter and multipes it to 1 to 12
+     System.out.println(j);
+   }
+     System.out.println("There is your multiplication table:)");
+
     
   }
 }
